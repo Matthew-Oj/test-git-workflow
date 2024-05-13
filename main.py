6ef11875a866b0d9a7b866211f6baac34f191d12
Matthew <matthew.oj@essencemediacom.com>
2024-05-13 16:54:38 +0100

    Add a image to main.py

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ st.title('Hello, Streamlit!')
 # Ask the user for their name
 user_name = st.text_input("What's your name?")
 
+st.image('linktoimage.png')
+
 # Greet the user
 if user_name:
-    st.write(f'Hello, {user_name! Welcome to your first Streamlit app!')
+    st.write(f'Hello, {user_name}! Welcome to your first Streamlit app!')
 
-# Run the app with: streamlit run hello_streamlit.py
+
